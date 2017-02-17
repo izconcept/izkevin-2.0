992b7fd7d889f91f50ce26517f4ff56cbd1033d0
izconcept <izkevin52@gmail.com>
2017-02-16 19:49:10 -0500

    Scrolling + Links to Projects
    -jQuery Scrolling from navbar
    -Correct repo and website links from projects
    -Improved content

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,12 @@
-/**
- * Created by IzKevin on 2017-02-16.
- */
+function scrollTo(id){
+    console.log(id);
+    $('html,body').animate({
+            scrollTop: $("#"+id).offset().top},
+        'slow');
+}
+
+/*
+$(document).ready(function() {
+    $('.parallelBG').parallax({imageSrc: 'media/bg6.jpg', naturalWidth: "1280", naturalHeight: "1280", speed: "0.1"});
+})
+    */
